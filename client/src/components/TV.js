0e6f50e6b6412d49c0e22cc45f432fdb3df53bda
Heather Parrott <briajade+github@gmail.com>
2018-12-25 10:58:54 -0500

    Update TV.js
    added style changes

--- client/src/components/TV.js
+++ client/src/components/TV.js
@@ -17,32 +17,45 @@ class TV extends Component {
         <div style={{ display: 'flex', fontSize: '34px' }}>
           <div style={{ flex: '1' }}>
             <p>
-              <strong>Score</strong>
+              <strong style="
+                fontSize: 27px;
+                >Score</strong>
               <ul
                 style={{ textAlign: 'left', width: '200px', margin: 'auto', listStyleType: 'none', fontSize: '20px' }}
               >
-                <li>Jason: 10000 pts</li>
+                <li style="
+                  fontSize: 22px;
+                  >Jason: 10000 pts</li>
                 <li>Heather: 5000 pts</li>
               </ul>
             </p>
             <p>
-              <strong>Last Clue (Clue 1 Across: Animal)</strong>
+              <strong style="
+                fontSize: 20px;
+                 >Last Clue: </strong>
+                 <br>
+                 <span style="
+                    fontSize: 23px;
+                 >(Clue 1 Across: Animal)</span>
               <ul
-                style={{ textAlign: 'left', width: '200px', margin: 'auto', listStyleType: 'none', fontSize: '20px' }}
+                style={{ textAlign: 'left', width: '200px', margin: 'auto', listStyleType: 'none', fontSize: '16px' }}
               >
                 <li>Jason: DOG (0 pts)</li>
                 <li>Heather: CAT (10 pts)</li>
               </ul>
             </p>
-            <p style={{ fontSize: '34px' }}>
+            <p style={{ fontSize: '44px' }}>
               <strong>
                 Clue 2 Across:
                 <br />
                 TV dinner guest
               </strong>
             </p>
-            <span style={{ textAlign: 'left', border: '1px black solid', padding: '5px' }}>:30</span>
-            <p>
+            <span style={{ textAlign: 'left', border: '1px black solid', padding: '10px' }}>:30</span>
+            <p style="
+                fontSize: 23px;
+                marginTop: 45px;
+              >
               <button is="google-cast-button" />
               <strong>Game ID: BLAH</strong>
             </p>
